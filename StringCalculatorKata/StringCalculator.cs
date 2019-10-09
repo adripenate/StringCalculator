4ac627103e6242214bc84b93c0c4a4b21f889d05
adripsau <adrianapenate98@gmail.com>
2019-10-09 10:09:36 +0100

    Refactor - Int32 expression simplified

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -6,7 +6,7 @@ namespace Kata
     {
         public static int Add(String numbers)
         {
-            return numbers.Length == 1 ? Int32.Parse(numbers) : 0;
+            return numbers.Length == 1 ? int.Parse(numbers) : 0;
         }
     }
 }
