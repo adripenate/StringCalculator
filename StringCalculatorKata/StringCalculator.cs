b8879b019d020aa76012962bd58006bca8bff8ab
adripsau <adrianapenate98@gmail.com>
2019-10-09 10:24:31 +0100

    Tests passed - return_sum_number_a_b_when_string_number_a_b

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -6,6 +6,16 @@ namespace Kata
     {
         public static int Add(String numbers)
         {
+            if(numbers.Length == 3)
+            {
+                String[] numbersSplit = numbers.Split(new String[] {","}, StringSplitOptions.None);
+                int result = 0;
+                for(int i = 0; i < 2; i++)
+                {
+                    result += int.Parse(numbersSplit[i]);
+                }
+                return result;
+            }
             return numbers.Length == 1 ? int.Parse(numbers) : 0;
         }
     }
