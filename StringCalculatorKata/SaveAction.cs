9cc3b983f76d82cedcb7b8f0a1255cd87add28a4
adripsau <adrianapenate98@gmail.com>
2019-10-11 11:34:45 +0100

    SaveActionShould and SaveAction classes added

---
+++ StringCalculatorKata/SaveAction.cs
@@ -0,0 +1,17 @@
+﻿using Persistence;
+using Model;
+
+namespace Kata
+{
+    public class SaveAction
+    {
+        private StringCalculator stringCalculator;
+        private IPersistence persistenceFile;
+
+        public SaveAction(StringCalculator stringCalculator, IPersistence persistenceFile)
+        {
+            this.stringCalculator = stringCalculator;
+            this.persistenceFile = persistenceFile;
+        }
+    }
+}
---
+++ Tests/SaveActionShould.cs
@@ -0,0 +1,30 @@
+﻿using Model;
+using Persistence;
+using Kata;
+using NUnit.Framework;
+using FluentAssertions;
+using System.IO;
+
+namespace Tests
+{
+    [TestFixture]
+    class SaveActionShould
+    {
+        private SaveAction saveAction;
+        private string path;
+
+        [SetUp]
+        public void SetUp()
+        {
+            saveAction = new SaveAction(new StringCalculator(), new PersistenceFile());
+            path = @"C:\Users\apenate\Desktop\StringCalculatorKata\LogTest.txt";
+            if (File.Exists(path)) File.Delete(path);
+        }
+
+        [Test]
+        public void write_operation_with_result_in_file()
+        {
+            
+        }
+    }
+}
