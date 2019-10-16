830184294bfacb353bfa3d0ba7f540b83a569291
adripsau <adrianapenate98@gmail.com>
2019-10-16 15:45:18 +0100

    Refactor - StringCalculator made magic strings and magic numbers as constants

--- Model/StringCalculator.cs
+++ Model/StringCalculator.cs
@@ -5,6 +5,12 @@ namespace Model
     public class StringCalculator
     {
         private const char DEFAULT_DELIMITER = ',';
+        private const string START_OF_DELIMITER_LINE = "//";
+        private const char NEW_LINE = '\n';
+        private const int MAX_NUMBER_VALUE = 1000;
+        private const int MIN_NUMBER_VALUE = 0;
+        private const int DELIMITER_POSITION = 2;
+        private const int START_OF_NUMBERS_LINE = 4;
 
         public int Add(String numbers)
         {
@@ -16,22 +22,22 @@ namespace Model
 
         private static char extractDelimiter(string numbers)
         {
-            return hasSpecialDelimiter(numbers) ? numbers[2] : DEFAULT_DELIMITER;
+            return hasSpecialDelimiter(numbers) ? numbers[DELIMITER_POSITION] : DEFAULT_DELIMITER;
         }
 
         private static string getWithoutDelimiterLine(string numbers)
         {
-            return hasSpecialDelimiter(numbers) ? numbers.Substring(4) : numbers;
+            return hasSpecialDelimiter(numbers) ? numbers.Substring(START_OF_NUMBERS_LINE) : numbers;
         }
 
         private static bool hasSpecialDelimiter(string numbers)
         {
-            return numbers.Contains("//");
+            return numbers.Contains(START_OF_DELIMITER_LINE);
         }
 
         private static String getCorrectFormat(string numbers, char delimiter)
         {
-            return numbers.Replace('\n', delimiter);
+            return numbers.Replace(NEW_LINE, delimiter);
         }
 
         private static string[] separateNumbers(string numbers, char delimiter)
@@ -63,12 +69,12 @@ namespace Model
 
         private static bool isNotBiggerThan1000(string number)
         {
-            return valueOf(number) <= 1000;
+            return valueOf(number) <= MAX_NUMBER_VALUE;
         }
 
         private static bool isNegative(string number)
         {
-            return valueOf(number) < 0;
+            return valueOf(number) < MIN_NUMBER_VALUE;
         }
 
         private static int valueOf(String number)
