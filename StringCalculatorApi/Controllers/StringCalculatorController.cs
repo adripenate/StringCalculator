d45185726b7c7978699a37a7d7f5444d7175defb
adripsau <adrianapenate98@gmail.com>
2019-10-25 11:48:30 +0100

    Bad request message changed

--- StringCalculatorApi/Controllers/StringCalculatorController.cs
+++ StringCalculatorApi/Controllers/StringCalculatorController.cs
@@ -10,12 +10,15 @@ namespace StringCalculatorApi.Controllers
     [ApiController]
     public class StringCalculatorController : ControllerBase
     {
+        private string postErrorMessage =
+            "{ \"type\":\"https://tools.ietf.org/html/rfc7231#section-6.5.1 \", \"title\":\"Negative numbers not allowed\", \"status\":400, \"traceId\":\"|e2d90705-448fd768e0a87daa.\" }";
+
         [HttpPost]
         public ActionResult<StringCalculatorResult> PostStringCalculator(StringCalculatorModel stringCalculatorModel)
         {
             var saveAction = new SaveAction(new StringCalculator(), new PersistenceFile(@"..\OperationLog.txt"));
             var stringCalculatorResult = saveAction.execute(stringCalculatorModel.Numbers);
-            return stringCalculatorResult ?? (ActionResult<StringCalculatorResult>) BadRequest();
+            return stringCalculatorResult ?? (ActionResult<StringCalculatorResult>) BadRequest(postErrorMessage);
         }
     }
 }
