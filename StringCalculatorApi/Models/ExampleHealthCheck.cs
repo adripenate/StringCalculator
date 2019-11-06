fd9887d077d2c77e0d3fa3fff81a20f6d2cc9794
adripsau <adrianapenate98@gmail.com>
2019-11-06 14:46:22 +0000

    HealthCheck added

---
+++ StringCalculatorApi/Models/ExampleHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Security.Permissions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+public class ExampleHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default(CancellationToken))
+    {
+        string fileLocation = @"..\OperationLog.txt";
+        using (var fs = new FileStream(fileLocation, FileMode.Open))
+        {
+            if (fs.CanWrite)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("File has writing permissions"));
+            }
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("File doesn't have writing permissions"));
+        }
+    }
+}
--- StringCalculatorApi/Startup.cs
+++ StringCalculatorApi/Startup.cs
@@ -1,13 +1,21 @@
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.PlatformAbstractions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace StringCalculatorApi
@@ -24,7 +32,7 @@ namespace StringCalculatorApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry(Configuration["ApplicationInsights:InstrumentationKey"]);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSwaggerGen(c =>
             {
@@ -46,8 +54,8 @@ namespace StringCalculatorApi
                     c.IncludeXmlComments(xmlPath);
                 }
             });
-
-
+            services.AddHealthChecks()
+                .AddCheck<ExampleHealthCheck>("example_health_check");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,11 +76,30 @@ namespace StringCalculatorApi
                 c.RoutePrefix = string.Empty;
             });
 
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                ResponseWriter = WriteResponse
+            });
+
             app.UseSwagger();
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "StringCalculator API API V1"); });
             app.UseHttpsRedirection();
             app.UseMvc();
             
+        }
+
+        private static Task WriteResponse(HttpContext httpContext, HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description)))))));
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
         }
     }
 }
